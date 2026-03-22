6029ed06f5c998ae397b90f1ae1e38ee29e8f0eb
shabinx30 <shabeensharih@gmail.com>
2026-03-22 14:14:17 +0530

    feat: 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,34 @@
+"use client";
+
 import FuzzyText from "@/components/FuzzyText";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const NotFound = () => {
+    const [time, setTime] = useState(3);
+    const router = useRouter();
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime((p) => {
+                if (p === 1) {
+                    clearInterval(interval);
+                    return 0;
+                }
+                return p - 1;
+            });
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        if (time === 0) {
+            router.push("/");
+        }
+    }, [time, router]);
+
     return (
         <div className="flex flex-col gap-6 justify-center items-center h-screen">
             <FuzzyText
@@ -12,7 +40,13 @@ const NotFound = () => {
                 404
             </FuzzyText>
             <h1 className="notable text-[#FFC7C7]">Page not found</h1>
-            <p className="mt-20 text-[#FFC7C7]">We'll redirect to you home</p>
+            <p className="mt-20 text-[#FFC7C7]">
+                We&apos;ll redirect you to{" "}
+                <Link href="/" className="underline">
+                    home
+                </Link>{" "}
+                in {time} seconds
+            </p>
         </div>
     );
 };
